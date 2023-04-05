0a91bed576c4e35f3cee92b852bd18bf9ce10840
Dragoon Aethis <rk@dragonic.eu>
2023-04-05 14:22:03 +0000

    Set UID/GIDs on the workspace after hooks finish
    Because the Docker runner uses a different user/group than the Jenkins
    agent or the test user, the final workspace directory may have invalid
    UID/GIDs after hooks finish. Force set them back to the initial value.

--- run-hooks.sh
+++ run-hooks.sh
@@ -1,2 +1,22 @@
 #!/usr/bin/bash
+
+cleanup() {
+    retcode=$?
+    workdir="$1"
+
+    # Because Docker, we need to chown external things back at exit
+    if [[ -d "$workdir" && "$(pwd)" == "$workdir" ]]; then
+        current_ugid="$(id -u):$(id -g)"
+        target_ugid="$(stat -c "%u:%g" "$workdir")"
+
+        if [[ "$current_ugid" != "$target_ugid" ]]; then
+            chown -R "$target_ugid" "$workdir"
+        fi
+    fi
+
+    exit $retcode
+}
+
+trap "cleanup /workspace" EXIT
+
 run-parts --exit-on-error --verbose "$(dirname $0)/hooks"
